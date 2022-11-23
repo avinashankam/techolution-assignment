9e9be8134daf91242a0d1b815cb58daccc91d654
avinashankam <avinashankam974@gmail.com>
2022-11-23 14:49:00 +0530

    fixes

--- apps/first/src/app/home/home.component.ts
+++ apps/first/src/app/home/home.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'first-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private router: Router) {}
+
+  navigate(): void {
+    this.router.navigateByUrl('/s')
+  }
+}
--- libs/header/src/lib/header.module.ts
+++ libs/header/src/lib/header.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
 import { ColorComponent } from './color/color.component';
 
 @NgModule({
